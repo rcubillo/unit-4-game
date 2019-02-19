f77c2f16538b91588fac49d53b0005e190fbd144
Chino <ronaldcubillo92@gmail.com>
2019-02-18 20:42:30 -0600

    random number was created for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,51 @@
 //   Variables
   var score =0;
- var numbers= [];
-  var value= ["4","7","10","13"];
 
-//Getting a random value from an array
-  var random = numbers[Math.floor(Math.random() * numbers.length)];
+//Getting a random values
+  var random = Math.floor(Math.random() * 120) + 19; 
+  var randomcrys1 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys2 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys3 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys4 = Math.floor(Math.random() * 12) + 1;
+
   //showing the selected number on  the HTML page
-  console.log(random);
   $("#score").text(random);
   $("#total").text(score);
-
- //Getting a random value from an array
- var randomvalue = value[Math.floor(Math.random() * value.length)];
- console.log(randomvalue);
-
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $(".crystal-one").on("click", function() {
      // Clicking the button triggers an alert message.
     alert("You clicked a crystal!");
-    console.log(score);
-    score += 4;
+    score += (randomcrys1);
     $("#total").text(score);
+    console.log(randomcrys1);
  });
-
+ 
    // Here we created an on-click event that responds to button clicks of the crystal image.
    $(".crystal-two").on("click", function() {
     // Clicking the button triggers an alert message.
    alert("You clicked a crystal!");
-   console.log("hola2");
-   score += 10;
+   score += (randomcrys2);
    $("#total").text(score);
+   console.log(randomcrys2);
 });
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $(".crystal-three").on("click", function() {
     // Clicking the button triggers an alert message.
    alert("You clicked a crystal!");
-   console.log("hola3");
-   score += 13;
+   score += (randomcrys3);
    $("#total").text(score);
-   ("#total").text(score);
+   console.log(randomcrys3);
 });
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $(".crystal-four").on("click", function() {
     // Clicking the button triggers an alert message.
    alert("You clicked a crystal!");
-   console.log("hola4");
-   score+= 15;
+   score+= (randomcrys4 );
    $("#total").text(score);
+   console.log(randomcrys4);
 });
 
 $(".card-img-top").on("click", function() {
