5bd80f75c675baab8fd3b0a23af425e1be9f9619
Chino <ronaldcubillo92@gmail.com>
2019-02-19 20:50:21 -0600

    adding the conditional ternary

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,21 @@
 //   Variables
   var score =0;
-
+  score = parseInt(score);
 //Getting a random values
-  var random = Math.floor(Math.random() * 120) + 19; 
+  var random = getRndInteger(19, 120); 
   var randomcrys1 = Math.floor(Math.random() * 12) + 1;
   var randomcrys2 = Math.floor(Math.random() * 12) + 1;
   var randomcrys3 = Math.floor(Math.random() * 12) + 1;
   var randomcrys4 = Math.floor(Math.random() * 12) + 1;
 
-  //showing the selected number on  the HTML page
+  //showing the selected number to guess.
   $("#score").text(random);
+  //showing the total score during the game.
   $("#total").text(score);
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-one").on("click", function() {
+   $(".crystal-one").on("click", function() {
      // Clicking the button triggers an alert message.
-    alert("You clicked a crystal!");
     score += (randomcrys1);
     $("#total").text(score);
     console.log(randomcrys1);
@@ -24,7 +24,6 @@
    // Here we created an on-click event that responds to button clicks of the crystal image.
    $(".crystal-two").on("click", function() {
     // Clicking the button triggers an alert message.
-   alert("You clicked a crystal!");
    score += (randomcrys2);
    $("#total").text(score);
    console.log(randomcrys2);
@@ -33,7 +32,6 @@
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $(".crystal-three").on("click", function() {
     // Clicking the button triggers an alert message.
-   alert("You clicked a crystal!");
    score += (randomcrys3);
    $("#total").text(score);
    console.log(randomcrys3);
@@ -42,18 +40,26 @@
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $(".crystal-four").on("click", function() {
     // Clicking the button triggers an alert message.
-   alert("You clicked a crystal!");
    score+= (randomcrys4 );
    $("#total").text(score);
    console.log(randomcrys4);
 });
 
-$(".card-img-top").on("click", function() {
-if (score === random) {
-    alert("You win!"); 
-  }
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 
-  else if (score >= random) {
-    alert("You lose!!");
-  }
-}); 
+
+$(".card").on("click", function() {
+  alert("New score: " + score);
+  // (score === random) && alert("You win!") || (score >= random) && alert("You lose!!") ;
+  //condition ? exprT : exprF 
+  console.log('random: ', random);
+  console.log('score: ', score);
+  score === random ? alert("You win!") : score >= random ? alert("You lose!!") : console.log('score less than random');
+  
+});
+
+function reset (){
+
+};
