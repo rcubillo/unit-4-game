1339743161f0a60d13829d06bad6767c86c25871
Chino <ronaldcubillo92@gmail.com>
2019-02-21 13:38:27 -0600

    Added three new functions (restart,win, losses)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,119 @@
+//this will ensure that jQuery functions won't fail due to unloaded pages.
 $(document).ready(function () {
 
   //variables
   var wins = 0;
   var losses = 0;
   var counter = 0;
-  
-  //Getting a random values
-  var random = getRndInteger(19, 120);
-  var randomcrys1 = Math.floor(Math.random() * 12) + 1;
-  var randomcrys2 = Math.floor(Math.random() * 12) + 1;
-  var randomcrys3 = Math.floor(Math.random() * 12) + 1;
-  var randomcrys4 = Math.floor(Math.random() * 12) + 1;
-  var randomcrys5 = getRndInteger(19, 120);
 
+  //DOM manipulation
+  $("#counter").text(counter);
+  $("#wins").text(wins);
+  $("#losses").text(losses);
 
-  //showing the selected number to guess.
-  $("#score").text(random);
-  //showing the total score during the game.
-  $("#total").text(score);
-
-
-  // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-one").on("click", function () {
-    // Clicking the button triggers an alert message.
-    score += (randomcrys1);
-    $("#total").text(score);
-    console.log(randomcrys1);
-  });
-
-  // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-two").on("click", function () {
-    // Clicking the button triggers an alert message.
-    score += (randomcrys2);
-    $("#total").text(score);
-    console.log(randomcrys2);
-  });
-
-  // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-three").on("click", function () {
-    // Clicking the button triggers an alert message.
-    score += (randomcrys3);
-    $("#total").text(score);
-    console.log(randomcrys3);
-  });
-
-  // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-four").on("click", function () {
-    // Clicking the button triggers an alert message.
-    score += (randomcrys4);
-    $("#total").text(score);
-    console.log(randomcrys4);
-  });
+  //getting a random number between 19- 120
+  var targetNumber = getRndInteger(19, 120);
 
   function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  // DOM manipulation
+  $("#number-to-guess").text(targetNumber);
+  //Getting random number
+  var randomcrys1 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys2 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys3 = Math.floor(Math.random() * 12) + 1;
+  var randomcrys4 = Math.floor(Math.random() * 12) + 1;
 
-  $(".card").on("click", function () {
-    alert("New score: " + score);
-    // (score === random) && alert("You win!") || (score >= random) && alert("You lose!!") ;
-    //condition ? exprT : exprF 
-    console.log('random: ', random);
-    console.log('score: ', score);
-    //score === random ? alert("You win!") : score >= random ? alert("You lose!!") : console.log('score less than random');
-    if (score >= random) {
-      $("#score").text(randomcrys5);
-      alert("You lose!!");
-      score = 0;
+  //Restarting the game function
+  function restart() {
+    targetNumber = getRndInteger(19, 120);
+
+    function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
     }
-    else if (score === random) {
-      $("#score").text(randomcrys5);
-      alert("You win!");
-      score = 0;
+    $('#number-to-guess').text(targetNumber);
+
+    randomcrys1 = Math.floor(Math.random() * 12) + 1;
+    randomcrys2 = Math.floor(Math.random() * 12) + 1;
+    randomcrys3 = Math.floor(Math.random() * 12) + 1;
+    randomcrys4 = Math.floor(Math.random() * 12) + 1;
+    counter = 0;
+    $("#counter").text(counter);
+  }
+
+  //Winning the game  function.
+  function winning() {
+    alert("You won!");
+    wins++;
+    $("#wins").text(wins);
+    restart();
+  }
+
+  //Lossing the  game
+  function lossing() {
+    alert("You lose");
+    losses++;
+    $("#losses").text(losses);
+    restart();
+  }
+  $(".crystal-one").on("click", function () {
+    // Clicking the button triggers an alert message.
+    counter = counter + (randomcrys1);
+    $("#counter").text(counter);
+    console.log(randomcrys1);
+    //counter == targetNumber ? winning() : counter > targetNumber ? lossing() : console.log('score less than random');
+    if (counter == targetNumber) {
+      winning();
     }
-    else {
-      console.log("test");
+    else if (counter > targetNumber) {
+      lossing();
+    }
+  });
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $(".crystal-two").on("click", function () {
+    // Clicking the button triggers an alert message.
+    counter += (randomcrys2);
+    $("#counter").text(counter);
+    console.log(randomcrys2);
+    //counter == targetNumber ? winning() : counter > targetNumber ? lossing() : console.log('score less than random');
+    if (counter == targetNumber) {
+      winning();
+    }
+    else if (counter > targetNumber) {
+      lossing();
+    }
+  });
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $(".crystal-three").on("click", function () {
+    // Clicking the button triggers an alert message.
+    counter += (randomcrys3);
+    $("#counter").text(counter);
+    console.log(randomcrys3);
+    //counter == targetNumber ? winning() : counter > targetNumber ? lossing() : console.log('score less than random');
+    if (counter == targetNumber) {
+      winning();
+    }
+    else if (counter > targetNumber) {
+      lossing();
+    }
+  });
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $(".crystal-four").on("click", function () {
+    // Clicking the button triggers an alert message.
+    counter += (randomcrys4);
+    $("#counter").text(counter);
+    console.log(randomcrys4);
+    //counter == targetNumber ? winning() : counter >  targetNumber ? lossing() : console.log('score less than random');
+    if (counter == targetNumber) {
+      winning();
+    }
+    else if (counter > targetNumber) {
+      lossing();
     }
   });
 });
